261c26149d23d82ece4a3461d99b269ed0e4066b
maisur <muhammad.rahman2@gmail.com>
2023-08-20 22:02:06 +0700

    date_added become date

--- app/Filament/Resources/ExpenseResource.php
+++ app/Filament/Resources/ExpenseResource.php
@@ -55,7 +55,7 @@ class ExpenseResource extends Resource
                     ->money('IDR')
                     ->sortable(),
                 Tables\Columns\TextColumn::make('date_added')
-                    ->dateTime()
+                    ->date()
                     ->sortable(),
             ])
             ->filters([
