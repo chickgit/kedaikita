ff3bf1aac60758142cbbf47c00cf2cb5da47bf24
maisur <muhammad.rahman2@gmail.com>
2023-08-20 22:12:10 +0700

    income & expense widget become one widget

--- app/Filament/Widgets/IncomeExpenseWidget.php
+++ app/Filament/Widgets/IncomeExpenseWidget.php
@@ -3,14 +3,27 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Customer;
+use App\Models\Expense;
 use App\Models\Order;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
-class IncomeWidget extends BaseWidget
+class IncomeExpenseWidget extends BaseWidget
 {
     protected function getStats(): array
     {
+        $todayExpenses = Expense::query()
+            ->where('date_added', now()->format('Y-m-d'))
+            ->get();
+
+        $weeklyExpenses = Expense::query()
+            ->whereBetween('date_added', [now()->startOfWeek(), now()->endOfWeek()])
+            ->get('amount');
+
+        $monthlyExpenses = Expense::query()
+            ->whereBetween('date_added', [now()->startOfMonth(), now()->endOfMonth()])
+            ->get('amount');
+
         $todayOrders = Order::query()
             ->where('date_order', now()->format('Y-m-d'))
             ->get('total_price');
@@ -27,6 +40,9 @@ class IncomeWidget extends BaseWidget
             Stat::make('Today Income', $todayOrders->sum('total_price')),
             Stat::make('This Week Income', $weeklyOrders->sum('total_price')),
             Stat::make('This Month Income', $monthlyOrders->sum('total_price')),
+            Stat::make('Today Expense', $todayExpenses->sum('amount')),
+            Stat::make('This Week Expense', $weeklyExpenses->sum('amount')),
+            Stat::make('This Month Expense', $monthlyExpenses->sum('amount')),
         ];
     }
 }
